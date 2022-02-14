405b17bf74dacc842707d0733e514f986f8e33ca
mafiyoung <65676596+mafiyoung@users.noreply.github.com>
2022-02-14 15:00:43 -0800

    added header component and links to home page, womens, mens, kids, shop and contact page

--- matt_apparel/matt-app/src/App.js
+++ matt_apparel/matt-app/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
-
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <Router>
     <div> 
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ matt_apparel/matt-app/src/components/header/header.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/matts_1.svg';
+import './header.styles.scss';
+
+const Header = () => (
+    <div className = 'header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo'/>
+        </Link>
+     <div className='options'>
+     <Link className='option' to ='/shop'>
+            SHOP
+        </Link>
+        <Link className='option' to ='/shop'>
+            WOMENS
+        </Link>
+        <Link className='option' to ='/shop'>
+            MENS
+        </Link>
+        <Link className='option' to ='/shop'>
+            KIDS
+        </Link>
+        <Link className='option' to ='/shop'>
+            CONTACT
+        </Link>
+      </div>
+    </div>
+)
+
+export default Header;
